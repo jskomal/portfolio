8bf4f786f7875b7fefc6b02abf45c30441484698
Jordan Skomal <jskomal@gmail.com>
2022-10-30 22:10:43 -0600

    Create page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,55 @@
 import { useState } from 'react'
 import { Typewriter } from 'react-simple-typewriter'
 
+import { links } from './assets/links'
+import gitHub from './assets/images/github.svg'
+import deploy from './assets/images/deploy-svgrepo-com.svg'
+
 import './App.css'
 
 function App() {
+  const [linkIndex, setLinkIndex] = useState(0)
+
+  const handleSlides = () => {
+    if (linkIndex === links.length - 1) {
+      setLinkIndex(0)
+    } else {
+      setLinkIndex((prev) => prev + 1)
+    }
+  }
+
   return (
-    <div className='App'>
+    <>
       <h1>
         <Typewriter words={["Hello there, I'm Jordan."]} />
       </h1>
-      <p>Front End Software Engineer </p>
-    </div>
+      <p>Front End Software Engineer</p>
+
+      <div className='project'>
+        <h2>{links[linkIndex].title}</h2>
+        <p>{links[linkIndex].description}</p>
+        <img
+          className='preview-img'
+          src={links[linkIndex].previewImg}
+          alt={links[linkIndex].title}
+        />
+        <div className='buttons-row'>
+          <a href={links[linkIndex].githubURL}>
+            <img className='svg' src={gitHub} alt='To GitHub' />
+          </a>
+
+          <a href={links[linkIndex].deployURL}>
+            <img className='svg' src={deploy} alt='To Deploy Link' />
+          </a>
+          <button onClick={handleSlides}>Next Project</button>
+        </div>
+      </div>
+      <div className='contacts'>
+        <a href='https://github.com/jskomal'>GitHub</a>
+        <a href='https://www.linkedin.com/in/jskomal/'>LinkedIn</a>
+        <a href='mailto:jskomal@gmail.com'>Email me</a>
+      </div>
+    </>
   )
 }
 

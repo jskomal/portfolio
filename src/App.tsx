12d080715b87af54c182580a66342282e84d7f37
jskomal <90876852+jskomal@users.noreply.github.com>
2022-10-31 21:46:02 -0600

    Add deploy link to img

--- src/App.tsx
+++ src/App.tsx
@@ -8,52 +8,54 @@ import deploy from './assets/images/deploy-svgrepo-com.svg'
 import './App.css'
 
 function App() {
-  const [linkIndex, setLinkIndex] = useState(0)
+	const [linkIndex, setLinkIndex] = useState(0)
 
-  const handleSlides = () => {
-    if (linkIndex === links.length - 1) {
-      setLinkIndex(0)
-    } else {
-      setLinkIndex((prev) => prev + 1)
-    }
-  }
+	const handleSlides = () => {
+		if (linkIndex === links.length - 1) {
+			setLinkIndex(0)
+		} else {
+			setLinkIndex((prev) => prev + 1)
+		}
+	}
 
-  return (
-    <main>
-      <header>
-        <h1>
-          <Typewriter words={["Hello there, I'm Jordan."]} />
-        </h1>
-        <p>
-          <Typewriter words={['Front End Software Engineer']} />
-        </p>
-      </header>
-      <div className='project'>
-        <h2>{links[linkIndex].title}</h2>
-        <p>{links[linkIndex].description}</p>
-        <img
-          className='preview-img'
-          src={links[linkIndex].previewImg}
-          alt={links[linkIndex].title}
-        />
-        <div className='buttons-row'>
-          <a href={links[linkIndex].githubURL}>
-            <img className='svg' src={gitHub} alt='To GitHub' />
-          </a>
+	return (
+		<main>
+			<header>
+				<h1>
+					<Typewriter words={["Hello there, I'm Jordan."]} />
+				</h1>
+				<p>
+					<Typewriter words={['Front End Software Engineer']} />
+				</p>
+			</header>
+			<div className='project'>
+				<h2>{links[linkIndex].title}</h2>
+				<p>{links[linkIndex].description}</p>
+				<a className='img-link' href={links[linkIndex].deployURL}>
+					<img
+						className='preview-img'
+						src={links[linkIndex].previewImg}
+						alt={links[linkIndex].title}
+					/>
+				</a>
+				<div className='buttons-row'>
+					<a href={links[linkIndex].githubURL}>
+						<img className='svg' src={gitHub} alt='To GitHub' />
+					</a>
 
-          <a href={links[linkIndex].deployURL}>
-            <img className='svg' src={deploy} alt='To Deploy Link' />
-          </a>
-          <a onClick={handleSlides}>Next Project</a>
-        </div>
-      </div>
-      <footer>
-        <a href='https://github.com/jskomal'>GitHub</a>
-        <a href='https://www.linkedin.com/in/jskomal/'>LinkedIn</a>
-        <a href='mailto:jskomal@gmail.com'>Email</a>
-      </footer>
-    </main>
-  )
+					<a href={links[linkIndex].deployURL}>
+						<img className='svg' src={deploy} alt='To Deploy Link' />
+					</a>
+					<a onClick={handleSlides}>Next Project</a>
+				</div>
+			</div>
+			<footer>
+				<a href='https://github.com/jskomal'>GitHub</a>
+				<a href='https://www.linkedin.com/in/jskomal/'>LinkedIn</a>
+				<a href='mailto:jskomal@gmail.com'>Email</a>
+			</footer>
+		</main>
+	)
 }
 
 export default App

068081acb6dab0aaf046bf982b4cea0e8cadbebf
Jordan Skomal <jskomal@gmail.com>
2022-12-01 08:21:22 -0700

    Create routes for redirect and implement redirect

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
+import RollForInitiative from './redirect_components/RollForInitiative'
+import SampleSpace from './redirect_components/SampleSpace'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '/roll-for-initiative',
+    element: <RollForInitiative />
+  },
+  {
+    path: 'sample-space',
+    element: <SampleSpace />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 )

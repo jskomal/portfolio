16fb30f50c46245850de11f6886d863376f1aeea
jskomal <90876852+jskomal@users.noreply.github.com>
2022-12-01 18:18:45 -0700

    Fix path for sample-space

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './index.css'
-import Redirect from './Redirect'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import Redirect from './Redirect';
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />
-  },
-  {
-    path: '/roll-for-initiative',
-    element: <Redirect url='https://roll-for-initiative-jskomal.vercel.app/' />
-  },
-  {
-    path: 'sample-space',
-    element: <Redirect url='https://samplespace.surge.sh/' />
-  }
-])
+	{
+		path: '/',
+		element: <App />,
+	},
+	{
+		path: '/roll-for-initiative',
+		element: <Redirect url='https://roll-for-initiative-jskomal.vercel.app/' />,
+	},
+	{
+		path: '/sample-space',
+		element: <Redirect url='https://samplespace.surge.sh/' />,
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <RouterProvider router={router} />
-)
+	<RouterProvider router={router} />
+);

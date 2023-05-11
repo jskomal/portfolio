c26940e02c62c7f74fad03474678b57b57893042
jskomal <90876852+jskomal@users.noreply.github.com>
2023-05-10 19:09:36 -0600

    Add keys for maps

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -3,29 +3,29 @@ import github from './assets/images/github.svg'
 import deploy from './assets/images/open-website-window-with-arrow-to-the-left-svgrepo-com.svg'
 
 const Projects = () => {
-  return (
-    <>
-      <h2 className='bold'>Projects</h2>
-      {links.map((project) => (
-        <div className='project-card'>
-          <h3 className='project-title'>{project.title}</h3>
-          <div className='img-and-links'>
-            <img src={project.previewImg} className='preview-img' />
-            <div className='project-links'>
-              <a href={project.githubURL}>
-                <img src={github} alt='To Github' className='svg' />
-              </a>
-              <a href={project.deployURL}>
-                <img src={deploy} alt='To Deployed Version' className='svg' />
-              </a>
-            </div>
-          </div>
-          <p>{project.description}</p>
-          <p className='stack'>{project.stack}</p>
-        </div>
-      ))}
-    </>
-  )
+	return (
+		<>
+			<h2 className='bold'>Projects</h2>
+			{links.map((project) => (
+				<div className='project-card' key={project.id}>
+					<h3 className='project-title'>{project.title}</h3>
+					<div className='img-and-links'>
+						<img src={project.previewImg} className='preview-img' />
+						<div className='project-links'>
+							<a href={project.githubURL}>
+								<img src={github} alt='To Github' className='svg' />
+							</a>
+							<a href={project.deployURL}>
+								<img src={deploy} alt='To Deployed Version' className='svg' />
+							</a>
+						</div>
+					</div>
+					<p>{project.description}</p>
+					<p className='stack'>{project.stack}</p>
+				</div>
+			))}
+		</>
+	)
 }
 
 export default Projects
--- src/Skills.tsx
+++ src/Skills.tsx
@@ -10,37 +10,37 @@ import html from './assets/images/html-5-svgrepo-com.svg'
 import css from './assets/images/css-3-svgrepo-com.svg'
 
 type TSkills = {
-  img: string
-  name: string
+	img: string
+	name: string
 }
 
 const skills: TSkills[] = [
-  { img: ts, name: 'Typescript' },
-  { img: react, name: 'React' },
-  { img: node, name: 'Node.js' },
-  { img: python, name: 'Python' },
-  { img: go, name: 'Go' },
-  { img: aws, name: 'AWS' },
-  { img: cypress, name: 'Cypress' },
-  { img: graphql, name: 'Graph QL' },
-  { img: html, name: 'HTML' },
-  { img: css, name: 'CSS' }
+	{ img: ts, name: 'Typescript' },
+	{ img: react, name: 'React' },
+	{ img: node, name: 'Node.js' },
+	{ img: python, name: 'Python' },
+	{ img: go, name: 'Go' },
+	{ img: aws, name: 'AWS' },
+	{ img: cypress, name: 'Cypress' },
+	{ img: graphql, name: 'Graph QL' },
+	{ img: html, name: 'HTML' },
+	{ img: css, name: 'CSS' },
 ]
 
 const Skills = () => {
-  return (
-    <>
-      <h2 className='bold'>Skills</h2>
-      <ul className='skills-grid'>
-        {skills.map((skill) => (
-          <div>
-            <img src={skill.img} alt={skill.name} className='skills-badge' />
-            <p className='badge-text'>{skill.name}</p>
-          </div>
-        ))}
-      </ul>
-    </>
-  )
+	return (
+		<>
+			<h2 className='bold'>Skills</h2>
+			<div className='skills-grid'>
+				{skills.map((skill, idx) => (
+					<div key={idx}>
+						<img src={skill.img} alt={skill.name} className='skills-badge' />
+						<p className='badge-text'>{skill.name}</p>
+					</div>
+				))}
+			</div>
+		</>
+	)
 }
 
 export default Skills
